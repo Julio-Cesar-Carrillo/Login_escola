b8a259b347f2fe094fd24c752d55c675b034f8b9
dbuenaño <deibycesar10@gmail.com>
2024-10-07 17:47:03 +0200

    subida bdd

---
+++ bdd.sql
@@ -0,0 +1,32 @@
+create database db_escuela;
+use databse db_escuela;
+
+CREATE TABLE  tbl_usarios(
+
+id_user INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+nom_user VARCHAR(30) NOT NULL,
+email_user VARCHAR(100) NOT NULL,
+rol_user INT NOT NULL,
+pwd_user VARCHAR(100) NOT NULL
+
+
+
+);
+
+CREATE TABLE tbl_roles(
+
+id_rol INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+nom_rol VARCHAR(30) NOT NULL
+
+);
+
+
+ALTER TABLE tbl_usarios 
+ADD CONSTRAINT fk_usario_roles FOREIGN KEY (rol_user) REFERENCES tbl_roles(id_rol);
+INSERT INTO tbl_roles VALUES(
+    NULL,'admin'
+);
+
+INSERT INTO tbl_usarios VALUES(
+    NULL,'Juan', 'juan@gmail.com', 1, '1234'
+);
