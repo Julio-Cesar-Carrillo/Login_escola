1e2d2ef75bd6b80e0bab1d1538eed3e6cd7aa62d
Julio-Cesar-Carrillo <100006703.joan23@fje.edu>
2024-10-09 19:12:58 +0200

    validacion con el servidor

--- paginaWeb/procesos/validaciones.php
+++ paginaWeb/procesos/validaciones.php
@@ -9,7 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         header('Location: ../login.php?error=El campo usuario es obligatorio');
         exit();
     }
-    
+
     if (empty($contrasena)) {
         header('Location: ../login.php?error=El campo contraseña es obligatorio');
         exit();
@@ -21,7 +21,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 
     if ($usuario === $usuario_valido && $contrasena === $contrasena_valida) {
-        header('Location: ../conexion.php'); // Redirigir a un dashboard o página principal
+        include('conexion.php');
+        $consulta = $pdo->prepare("SELECT * FROM tbl_usuarios WHERE nom_user = :user");
+        $consulta->bindParam(":user", $user);
+        $consulta->execute();
+        $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
+
+        session_start();
+
+        if ($resultado && password_verify($pwd, $resultado['pwd_user'])) {
+            $session['nom'] = $resultado['nom_user'];
+            header("location: ../bienvenido.php");
+        } else {
+            header("location: ../login.php?nom=" . $user . "&pwd=" . $pwd);
+            exit();
+        }
         exit();
     } else {
         // Credenciales incorrectas
