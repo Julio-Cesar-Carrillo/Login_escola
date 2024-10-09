03b6615e9af2759209c3b5026eb7bd336d249157
PC-ERIC <eric.alcazarc@gmail.com>
2024-10-09 18:18:05 +0200

    Conexion modificado sin PDO

--- paginaWeb/procesos/conexion.php
+++ paginaWeb/procesos/conexion.php
@@ -1,9 +1,15 @@
 <?php
-$servidor = "mysql:dbname=db_escuela_login;host=localhost";
-$user = "root";
-$pass = "";
-try {
-    $pdo = new PDO($servidor, $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-} catch (PDOException $e) {
-    echo "conexion fallida" . $e->getMessage();
+$host = "localhost"; 
+$usuario = "root";
+$contraseña = "";
+$base_de_datos = "db_escuela"; 
+
+$conexion = new mysqli($host, $usuario, $contraseña, $base_de_datos);
+
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+} else {
+    echo "Conexión exitosa a la base de datos";
 }
+
+?>
